d8b31b8a8f34571e29335abce0949cba4a25cf1d
husseinx1970 <husseinmormor@gmail.com>
2025-10-12 19:59:33 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function draw() {
-    // Immediate drawing of the company name
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    // Assuming 'drawCompanyName' is the function that draws the name
-    drawCompanyName();
-    // ... other drawing logic if needed
-}
